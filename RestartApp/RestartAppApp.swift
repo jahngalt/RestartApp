15473d97ff7a8b324891bfb9cf8b9e060480627d
Oleg Kudimov <ovkudimov@gmail.com>
2022-01-12 21:59:35 +0300

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Oleg Kudimov on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
